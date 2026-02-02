710799a6684947b3ee0a1b73ecda3e659a44723f
BenjaminNiklasson <benjamin.niklasson@elev.ga.lbs.se>
2026-02-02 10:33:53 +0100

    ssss

--- Bloodrunner/Assets/Scripts/Globals/Globals.cs
+++ Bloodrunner/Assets/Scripts/Globals/Globals.cs
@@ -1,4 +1,5 @@
 using Unity.Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Globals : MonoBehaviour
@@ -6,6 +7,12 @@ public class Globals : MonoBehaviour
     public Vector3 respawnPoint { get; set; }
     [SerializeField] GameObject playerPrefab;
     public GameObject currentPlayer { get; set; }
+
+    public void Start()
+    {
+        Physics.IgnoreLayerCollision(3, 0);
+    }
+
     public void PlayerDeathGlobal()
     {
         currentPlayer.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
