90ea8cd8089bb7596bad7d7460a58aa3ed9f0442
BenjaminNiklasson <benjamin.niklasson@elev.ga.lbs.se>
2026-02-02 13:57:19 +0100

    sky fix

--- Bloodrunner/Assets/Scripts/Lava/SlowDownLava.cs
+++ Bloodrunner/Assets/Scripts/Lava/SlowDownLava.cs
@@ -3,19 +3,18 @@ using UnityEngine;
 public class SlowDownLava : MonoBehaviour
 {
     LavaRise lavaRise;
-    float lavaRiseSpeedOriginal;
+    [SerializeField] float lavaRiseSlowing = 3;
     bool lavaSlowed = false;
     private void Start()
     {
         lavaRise = GameObject.FindWithTag("Lava").GetComponent<LavaRise>();
-        lavaRiseSpeedOriginal = lavaRise.lavaRiseSpeed;
     }
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Lavahit!");
         if (other.transform.CompareTag("Lava") && lavaSlowed == false)
         {
-            lavaRise.lavaRiseSpeed = lavaRiseSpeedOriginal;
+            lavaRise.lavaRiseSpeed = lavaRise.lavaRiseSpeed/lavaRiseSlowing;
             lavaSlowed = true;
         }
     }
