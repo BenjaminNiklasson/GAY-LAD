3e9b143ce0ab1c8dcbd67343266e5b11df6d8d62
BenjaminNiklasson <benjamin.niklasson@elev.ga.lbs.se>
2026-02-04 14:42:57 +0100

    sss

--- Bloodrunner/Assets/Scripts/Objects/MaterialGlow.cs
+++ Bloodrunner/Assets/Scripts/Objects/MaterialGlow.cs
@@ -6,20 +6,21 @@ using UnityEngine.Rendering.HighDefinition;
 public class MaterialGlow : MonoBehaviour
 {
     [SerializeField] float _glowDivider;
+    [SerializeField] Color _emissionColor;
     Renderer _renderer;
     Material _material;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _renderer = gameObject.GetComponent<MeshRenderer>();
+        _renderer = gameObject.GetComponent<Renderer>();
         _material = _renderer.material;
-        _renderer.material = _material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _material.SetColor("_EmissionColor", _material.color * (_glowDivider / (transform.position.y - GameObject.FindGameObjectWithTag("Lava").transform.position.y)));
+        DynamicGI.SetEmissive(_renderer, _material.color * ((transform.position.y - GameObject.FindGameObjectWithTag("Lava").transform.position.y) / _glowDivider));
+
     }
 }
