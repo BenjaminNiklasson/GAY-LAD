bd441e7a2e1fde05f6e3f0691948010601557c8f
BenjaminNiklasson <benjamin.niklasson@elev.ga.lbs.se>
2026-02-05 11:29:49 +0100

    sss

--- Bloodrunner/Assets/Scripts/Player/PlayerMovement.cs
+++ Bloodrunner/Assets/Scripts/Player/PlayerMovement.cs
@@ -97,10 +97,16 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnZip()
     {
+        Debug.Log("Click");
         if (globals.seeHook)
         {
             //ZIPPING!!!
-            rb.AddForce();
+            rb.AddForce(globals.hookSeen.transform.position - transform.position, ForceMode.Impulse);
         }
     }
+
+    //public void OnPause()
+    //{
+    //    globals.PauseGame();
+    //}
 }
